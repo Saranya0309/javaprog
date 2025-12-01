1f8a41a9669c3be2212006f50662ac0e0f8ac1b4
Saranya0309 <smsaranya111@gmail.com>
2025-12-01 11:18:59 +0530

    Add files via upload

---
+++ Leapyrornot.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Leapyrornot
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    int year=sc.nextInt();
+	    if((year%4==0 &&year%100!=0) ||year%400==0)
+	    {
+	        System.out.print("It is a leap year");
+	    }
+	    else
+	    {
+	        System.out.print("It is not a leap year");
+	    }
+	    
+	}
+}
