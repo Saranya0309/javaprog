f1fce618c6a04d9ea63b29ee080ac9346c67cc8f
Saranya0309 <smsaranya111@gmail.com>
2025-12-02 09:12:23 +0530

    Add files via upload

---
+++ Numberrangecheck.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Numberrangecheck {
+    public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    int N=sc.nextInt();
+    int L=sc.nextInt();
+    int R=sc.nextInt();
+    
+    if(((N>L) && (N<R)) || ((N<L) && (N>R))  ){
+        System.out.println("yes");
+    }
+    else
+    {
+        System.out.println("no");
+    }
+    }
+}
